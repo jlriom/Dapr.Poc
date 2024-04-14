cfa176b12243735e14be0da20ec61bdc554a3096
Jose Luis DEL RIO MERCADAL <joseluis.delrio.externo@axa-assistance.es>
2024-04-14 10:41:02 +0200

    Agregar archivos de proyecto.

---
+++ Dapr.Poc.ConsoleClient/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Dapr.Client;
+
+Console.WriteLine("Hello, World!");
+
+
+const string storeName = "statestore";
+const string key = "counter";
+
+var daprClient = new DaprClientBuilder().Build();
+var counter = await daprClient.GetStateAsync<int>(storeName, key);
+while (true)
+{
+    Console.WriteLine($"Counter = {counter++}");
+    await daprClient.SaveStateAsync(storeName, key, counter);
+    await Task.Delay(1000);
+}
