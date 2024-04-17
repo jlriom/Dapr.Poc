c7726313450cd7ec8afbb74ee3a2b94c8f8eb402
Jose Luis DEL RIO MERCADAL <joseluis.delrio.externo@axa-assistance.es>
2024-04-17 23:04:34 +0200

    c402

---
+++ MyFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+
+        private readonly DaprClient _daprClient;
+
+        public IndexModel(ILogger<IndexModel> logger, DaprClient daprClient)
+        {
+            _logger = logger;
+            _daprClient = daprClient;
+        }
+
+        public async Task OnGet()
+        {
+            var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(HttpMethod.Get, "MyBackEnd", "weatherforecast");
+
+            ViewData["WeatherForecastData"] = forecasts;
+        }
+    }
+}
